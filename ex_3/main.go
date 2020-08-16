617bd30418cd1d7342e6f642e92c9026214046fc
leenorshn <katembovictor@gmail.com>
2020-08-16 12:24:33 +0100

    pointer to a struct

--- ex_3/main.go
+++ ex_3/main.go
@@ -16,15 +16,21 @@ type Car struct {
 }
 
 func main() {
-	var address Address
-	address = Address{name: "Auberge", street: "Mutsanga", city: "Butembo", state: "DRC", Pincode: 23}
-	c := Car{"Rav4", "Voiture", "Metalique", 5788.9}
-	fmt.Println(address)
-	fmt.Println(c.Color)
-	c.Model = "Toyota"
-	fmt.Println(c.Model)
+	// var address Address
+	// address = Address{name: "Auberge", street: "Mutsanga", city: "Butembo", state: "DRC", Pincode: 23}
+	// c := Car{"Rav4", "Voiture", "Metalique", 5788.9}
+	// fmt.Println(address)
+	// fmt.Println(c.Color)
+	// c.Model = "Toyota"
+	// fmt.Println(c.Model)
 
 	// for _, t := range address {
 	// 	fmt.Println(t)
 	// }
+
+	c := &Car{"Rav4", "Voiture", "Metalique", 5788}
+	fmt.Println(*c)
+	c2 := &c
+	(*c2).Model = "Hammer"
+	fmt.Println((*c).Model)
 }
